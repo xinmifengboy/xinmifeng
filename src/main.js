b91c6ab5e9620efa415257b5d3b54a6fbf6dab31
shuaiqiboy <16139991@qq.com>
2023-06-07 17:31:42 +0800

    feat: 安装three.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Author: shuaiqiboy 16139991@qq.com
  * @Date: 2023-06-07 14:57:52
  * @LastEditors: shuaiqiboy 16139991@qq.com
- * @LastEditTime: 2023-06-07 15:20:46
+ * @LastEditTime: 2023-06-07 17:29:39
  * @FilePath: /xinmifeng/src/main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,6 +11,9 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import './assets/iconfont/iconfont.css'
+import * as THREE from 'three'
+
+window.T3 = THREE
 
 const app = createApp(App)
 app.use(router)
