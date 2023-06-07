9d4ddeab6c312731c23dcaec77a147e03d44c3c9
comcjl052@qq.com <comcjl052@qq.com>
2023-06-07 15:55:26 +0800

    add file

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import './assets/iconfont/iconfont.css'
 
 const app = createApp(App)
 app.use(router)
