f03466639e0cc8fb542aad27ac260e333f063d18
comcjl052@qq.com <comcjl052@qq.com>
2023-06-08 09:40:49 +0800

    画线

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,29 +4,28 @@
 </template>
 
 <script setup>
+document.body.innerHTML = '' // 清空body内容
 const scene = new T3.Scene() //  创建场景
 
-const camera = new T3.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); //  创建相机
+const camera = new T3.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); //  创建相机
 
 const renderer = new T3.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new T3.BoxGeometry( 1, 1, 1 );
-const material = new T3.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new T3.Mesh( geometry, material );
-scene.add( cube );
+const geometry = new T3.BoxGeometry(1, 1, 1);
+const material = new T3.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new T3.Mesh(geometry, material);
+scene.add(cube);
 
 camera.position.z = 5;
 function animate() {
-	requestAnimationFrame( animate );
+  requestAnimationFrame(animate);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
 animate();
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,14 +8,16 @@
  */
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/pages/index.vue'
+import Lines from '@/pages/lines/index.vue'
 const routes = [
-  { path: '/', component: Home },
+    { path: '/', component: Home },
+    { path: '/lines', component: Lines },
 ]
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
-  scrollBehavior() { //  路由切换将页面置顶
-    return { x: 0, y: 0 }
-  }
+    history: createWebHistory(),
+    routes,
+    scrollBehavior() { //  路由切换将页面置顶
+        return { x: 0, y: 0 }
+    }
 })
 export default router
