46473b78b8c4e022e714bf049591767f561a006b
shuaiqiboy <16139991@qq.com>
2023-06-08 10:35:20 +0800

    feat: 添加工具类

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Author: shuaiqiboy 16139991@qq.com
  * @Date: 2023-06-07 14:57:52
  * @LastEditors: shuaiqiboy 16139991@qq.com
- * @LastEditTime: 2023-06-07 17:29:39
+ * @LastEditTime: 2023-06-08 10:33:07
  * @FilePath: /xinmifeng/src/main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,8 +12,11 @@ import App from './App.vue'
 import router from './router'
 import './assets/iconfont/iconfont.css'
 import * as THREE from 'three'
-
+import WebGL from '@/assets/js/WebGL.js'
+import * as Utils from '@/assets/js/utils.js'
 window.T3 = THREE
+window.WebGL = WebGL
+window.Utils = Utils
 
 const app = createApp(App)
 app.use(router)
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,3 @@
-/*
- * @Author: shuaiqiboy 16139991@qq.com
- * @Date: 2023-06-07 15:13:38
- * @LastEditors: shuaiqiboy 16139991@qq.com
- * @LastEditTime: 2023-06-07 15:19:44
- * @FilePath: /xinmifeng/src/router/index.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/pages/index.vue'
 import Lines from '@/pages/lines/index.vue'
